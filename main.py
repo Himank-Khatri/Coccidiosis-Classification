d27492797a39df7fba52ba0432428e3a3c8f88e7
Himank <k.himank.14@gmail.com>
2024-10-08 00:25:35 +0530

    updated data_ingestion.ipynb, constants, components, entity, pipeline, main, config

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from chickenDiseaseClassifier import logger
+from chickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
