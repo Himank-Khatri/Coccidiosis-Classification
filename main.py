15cb244ef9a820cbd029ac132f93124d73812f5f
Himank <k.himank.14@gmail.com>
2024-10-08 16:49:15 +0530

    added prepare_base_model config, entity, component, pipeline and added it to main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from chickenDiseaseClassifier import logger
 from chickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare base model stage"
+try:
+    logger.info("*****************************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
     raise e
